43525bc1de9671b5d891b1bfc7cbbb68ad05eb09
acm1026 <arturo1026@alu.ubu.es>
2023-03-31 19:06:54 +0200

    cambios

---
+++ src/p03/c01/IParque.java
@@ -0,0 +1,10 @@
+package src.p03.c01;
+
+public interface IParque {
+	
+	public abstract void entrarAlParque(String puerta);
+
+	public abstract void salirDelParque(String puerta);
+	// TODO
+	//me deja hacer commit
+}
